2920620d3f5aa2b4408e11467934721baba3371f
efibs <106489640+efibs@users.noreply.github.com>
2022-10-23 15:39:38 +0200

    Render vest lights emissive

--- src/main/java/de/kleiner3/lasertag/entity/render/armor/LasertagVestRenderer.java
+++ src/main/java/de/kleiner3/lasertag/entity/render/armor/LasertagVestRenderer.java
@@ -1,6 +1,8 @@
 package de.kleiner3.lasertag.entity.render.armor;
 
+import de.kleiner3.lasertag.LasertagMod;
 import de.kleiner3.lasertag.item.LasertagVestItem;
+import net.minecraft.client.render.OverlayTexture;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
@@ -8,12 +10,19 @@ import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Quaternion;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib3.core.processor.IBone;
 import software.bernie.geckolib3.core.util.Color;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
+import software.bernie.geckolib3.renderers.GeoRenderTypes;
 import software.bernie.geckolib3.renderers.geo.GeoArmorRenderer;
+import software.bernie.geckolib3.util.GeoUtils;
+import software.bernie.geckolib3.util.RenderUtils;
 
 public class LasertagVestRenderer extends GeoArmorRenderer<LasertagVestItem> {
 
@@ -36,10 +45,30 @@ public class LasertagVestRenderer extends GeoArmorRenderer<LasertagVestItem> {
     @Override
     public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, ItemStack stack, LivingEntity entity,
                        EquipmentSlot slot, int light, BipedEntityModel<LivingEntity> contextModel) {
-        setModel(VEST_MODEL);
         super.render(matrices, vertexConsumers, stack, entity, slot, light, contextModel);
-        setModel(LIGHTS_MODEL);
-        super.render(matrices, vertexConsumers, stack, entity, slot, light, contextModel);
+
+        RenderLayer cameo = RenderLayer.getEyes(LIGHTS_MODEL.getTextureResource(null));
+        matrices.push();
+
+        matrices.translate(0.0D, 1.497F, 0.0D);
+        matrices.scale(-1.005F, -1.0F, 1.005F);
+
+        var lightsModel = LIGHTS_MODEL.getModel(LIGHTS_MODEL.getModelResource(null));
+
+        if (this.bodyBone != null) {
+            IBone bodyBone = LIGHTS_MODEL.getBone(this.bodyBone);
+            GeoUtils.copyRotations(contextModel.body, bodyBone);
+            bodyBone.setPositionX(contextModel.body.pivotX);
+            bodyBone.setPositionY(-contextModel.body.pivotY);
+            bodyBone.setPositionZ(contextModel.body.pivotZ);
+        }
+
+        var color = ((LasertagVestItem)LasertagMod.LASERTAG_VEST).getColor(itemStack);
+        float r = ((color >> 16) & 0xFF) / 255.0F;
+        float g = ((color >> 8) & 0xFF) / 255.0F;
+        float b = ((color >> 0) & 0xFF) / 255.0F;
+        this.render(lightsModel, (LasertagVestItem) LasertagMod.LASERTAG_VEST, 1.0F, cameo, matrices, vertexConsumers, vertexConsumers.getBuffer(cameo), light, OverlayTexture.DEFAULT_UV, r, g, b, 1.0F);
+        matrices.pop();
     }
 
     @Override
