fb33abdbe947bd0b653ed9aaa85b95fa77207545
efibs <106489640+efibs@users.noreply.github.com>
2022-11-20 17:48:20 +0100

    Player spawn at 0 0 0

---
+++ src/main/java/de/kleiner3/lasertag/mixin/ServerWorldMixin.java
@@ -0,0 +1,28 @@
+package de.kleiner3.lasertag.mixin;
+
+import de.kleiner3.lasertag.worldgen.chunkgen.ArenaChunkGenerator;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.WorldGenerationProgressListener;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.dimension.DimensionOptions;
+import net.minecraft.world.level.ServerWorldProperties;
+import net.minecraft.world.level.storage.LevelStorage;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.concurrent.Executor;
+
+@Mixin(ServerWorld.class)
+public abstract class ServerWorldMixin {
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void atInit(MinecraftServer server, Executor workerExecutor, LevelStorage.Session session, ServerWorldProperties properties, RegistryKey worldKey, DimensionOptions dimensionOptions, WorldGenerationProgressListener worldGenerationProgressListener, boolean debugWorld, long seed, List spawners, boolean shouldTickTime, CallbackInfo ci) {
+        if (dimensionOptions.getChunkGenerator() instanceof ArenaChunkGenerator) {
+            ((ServerWorld)(Object)this).setSpawnPos(BlockPos.ORIGIN, 0.0F);
+        }
+    }
+}
