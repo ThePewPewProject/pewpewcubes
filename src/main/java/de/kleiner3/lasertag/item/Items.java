f4a983f7a7e751dd24c019e2edf8ce0034d46a25
efibs <106489640+efibs@users.noreply.github.com>
2023-05-20 22:04:12 +0200

    Set laservest durability to Int.MAX_VALUE

--- src/main/java/de/kleiner3/lasertag/item/Items.java
+++ src/main/java/de/kleiner3/lasertag/item/Items.java
@@ -2,8 +2,9 @@ package de.kleiner3.lasertag.item;
 
 import de.kleiner3.lasertag.LasertagMod;
 import de.kleiner3.lasertag.block.Blocks;
+import de.kleiner3.lasertag.item.material.LaserVestMaterial;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.ArmorMaterials;
+import net.minecraft.item.ArmorMaterial;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
@@ -16,12 +17,17 @@ import net.minecraft.util.registry.Registry;
  * @author Étienne Muser
  */
 public class Items {
+
+    // Create instances for all materials
+    public static final ArmorMaterial LASERVEST_MATERIAL = new LaserVestMaterial();
+
     // Create instances for all items
     public static final Item LASERTAG_WEAPON = new LasertagWeaponItem(new FabricItemSettings().group(ItemGroups.LASERTAG_ITEM_GROUP).maxCount(1).rarity(Rarity.EPIC));
-    public static final Item LASERTAG_VEST = new LasertagVestItem(ArmorMaterials.LEATHER, new FabricItemSettings().group(ItemGroups.LASERTAG_ITEM_GROUP).rarity(Rarity.EPIC));
+    public static final Item LASERTAG_VEST = new LasertagVestItem(LASERVEST_MATERIAL, new FabricItemSettings().group(ItemGroups.LASERTAG_ITEM_GROUP).rarity(Rarity.EPIC));
 
 
     public static void register() {
+
         // Block items
         Registry.register(Registry.ITEM, new Identifier(LasertagMod.ID, "arena_block"),
                 new BlockItem(Blocks.ARENA_BLOCK, new FabricItemSettings().group(ItemGroups.LASERTAG_ITEM_GROUP)));
