535a5b753affcdb9eb317daa30329a23695b5b9e
Der_fbs <106489640+efibs@users.noreply.github.com>
2023-10-09 22:12:27 +0200

    Player can't take off vest

---
+++ src/main/java/de/kleiner3/lasertag/mixin/SlotMixin.java
@@ -0,0 +1,28 @@
+package de.kleiner3.lasertag.mixin;
+
+import de.kleiner3.lasertag.item.Items;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.screen.slot.Slot;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+/**
+ * Mixin into the slot class to make lasertag vests effectively have curse of binding. The player can't take it off.
+ *
+ * @author Étienne Muser
+ */
+@Mixin(Slot.class)
+public abstract class SlotMixin {
+
+    @Inject(method = "canTakeItems(Lnet/minecraft/entity/player/PlayerEntity;)Z", at = @At("HEAD"), cancellable = true)
+    private void onCanTakeItems(PlayerEntity playerEntity, CallbackInfoReturnable<Boolean> cir) {
+
+        // Get the item stack of the slot
+        var itemStack = ((Slot)(Object)this).getStack();
+
+        // Player can always take it of if he is in creative mode. But otherwise can't take of the lasertag vest.
+        cir.setReturnValue(playerEntity.isCreative() || !itemStack.isOf(Items.LASERTAG_VEST));
+    }
+}
