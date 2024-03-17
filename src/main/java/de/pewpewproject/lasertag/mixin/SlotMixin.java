dc99b4ca455cba0cbaedb74af8ecfa4cc1237c8c
efibs <etienne.muser@hotmail.com>
2024-03-17 21:02:36 +0100

    Prevent player putting away weapon into chests or similar stuff

--- src/main/java/de/pewpewproject/lasertag/mixin/SlotMixin.java
+++ src/main/java/de/pewpewproject/lasertag/mixin/SlotMixin.java
@@ -2,6 +2,8 @@ package de.pewpewproject.lasertag.mixin;
 
 import de.pewpewproject.lasertag.item.Items;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.slot.Slot;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,6 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Mixin into the slot class to make lasertag vests effectively have curse of binding. The player can't take it off.
+ * As well as being unable to put away the weapon.
  *
  * @author Étienne Muser
  */
@@ -24,5 +27,26 @@ public abstract class SlotMixin {
 
         // Player can always take it of if he is in creative mode. But otherwise can't take of the lasertag vest.
         cir.setReturnValue(playerEntity.isCreative() || !itemStack.isOf(Items.LASERTAG_VEST));
+    }
+
+    @Inject(method = "canInsert(Lnet/minecraft/item/ItemStack;)Z", at = @At("RETURN"), cancellable = true)
+    private void onCanInsert(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
+
+        // If the inventory is the players inventory
+        if (((Slot)(Object)this).inventory instanceof PlayerInventory) {
+
+            // Player can move weapon in his inventory
+            return;
+        }
+
+        // If the item he is moving is not the weapon
+        if (!stack.isOf(Items.LASERTAG_WEAPON)) {
+
+            // Player can move other items
+            return;
+        }
+
+        // No moving
+        cir.setReturnValue(false);
     }
 }
